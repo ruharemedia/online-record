803561617b02d14c34a7c7c8898d75cb42a20d47
ZemzerovIA <ruharepro@gmail.com>
2024-07-29 21:02:18 +0300

    [FEATURE] create cell service and spread items

---
+++ app/Services/CellIntervalService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services;
+
+use Carbon\CarbonPeriod;
+
+class CellIntervalService{
+
+    /**
+     * Method create time cells
+     *
+     * @param int $interval = 30     - interval of time;
+     * @param string $startTime - start interval time;
+     * @param string $endTime   - end interval time;
+     *
+     * @return array
+     */
+
+    //TODO есть проблема с последним временем
+    public static function createCells(int $interval, string $startTime, string $endTime) :mixed
+    {
+        $fullDates = CarbonPeriod::create($startTime, "$interval minutes", $endTime);
+
+        $hoursAndMinutes = [];
+
+        foreach ($fullDates as $cell){
+            $hoursAndMinutes[] = $cell->format('H:i');
+        }
+
+        $cells = array();
+
+        foreach ($hoursAndMinutes as $index => $value) {
+
+            if(isset($hoursAndMinutes[$index+1])){
+                $cells[] = [
+                    $value,
+                    $hoursAndMinutes[$index+1]
+                ];
+            }
+        }
+
+        return $cells;
+    }
+}
