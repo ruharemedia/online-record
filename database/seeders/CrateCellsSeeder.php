d5a6df6f66ebce69d5cbf66734e1854317c078dd
ZemzerovIA <ruharepro@gmail.com>
2024-07-29 21:02:41 +0300

    [FEATURE] extend cellsseeder

--- database/seeders/CrateCellsSeeder.php
+++ database/seeders/CrateCellsSeeder.php
@@ -3,44 +3,20 @@
 namespace Database\Seeders;
 
 use App\Models\RecordCells;
+use App\Services\CellIntervalService;
 use Illuminate\Database\Seeder;
 
 class CrateCellsSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Seeder create default cells
      */
     public function run(): void
     {
-        $interval = 30;
-        $cells = [
-            [
-                "started_at" => '08:00',
-                "end_at" => '08:30'
-            ],
-            [
-                "started_at" => '08:30',
-                "end_at" => '09:00'
-            ],
-            [
-                "started_at" => '09:00',
-                "end_at" => '09:30'
-            ],
-            [
-                "started_at" => '09:30',
-                "end_at" => '10:00'
-            ],
-            [
-                "started_at" => '10:00',
-                "end_at" => '10:30'
-            ],
-            [
-                "started_at" => '10:30',
-                "end_at" => '11:00'
-            ],
-        ];
-        foreach ($cells as $cell){
-            RecordCells::create($cell);
+        $cells = CellIntervalService::createCells(30, '09:00', "19:00");
+
+        foreach ($cells as $item){
+            RecordCells::create(['started_at' => $item[0], 'end_at' => $item[1]]);
         }
     }
 }
